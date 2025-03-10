8db0251b3538f0be11e56287d7faec2647a4c96a
faiz <khanfaizmominshama@gmail.com>
2025-03-10 20:44:11 +0530

    Added the Drag and drop option and the file gets saves in the CWD

--- code/frontend/app.py
+++ code/frontend/app.py
@@ -1,1 +1,21 @@
+import os
 import streamlit as st
+import uuid
+
+def main():
+    uploaded=st.file_uploader("UPLOAD HERE :)")    
+    
+    if uploaded:
+        uID=uuid.uuid4().hex[:8]
+        
+        filepath=os.path.join(os.getcwd(), f"{uploaded.name}_{uID}")
+        
+        with open(filepath, 'wb') as f:
+            f.write(uploaded.getbuffer())
+        
+    
+
+
+if __name__=="__main__":
+    main()
+
